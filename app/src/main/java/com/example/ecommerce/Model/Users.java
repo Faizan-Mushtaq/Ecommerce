1bf4da8b85c1979cdafa2c72feae9b2707e97b6a
Faizan-Mushtaq <faizan.wani234@gmail.com>
2020-03-11 16:16:14 +0530

    Add Project

---
+++ app/src/main/java/com/example/ecommerce/Model/Users.java
@@ -0,0 +1,38 @@
+package com.example.ecommerce.Model;
+
+public class Users {
+    private String phone,name,password;
+    public Users(){
+
+    }
+
+    public Users(String phone, String name, String password) {
+        this.phone = phone;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

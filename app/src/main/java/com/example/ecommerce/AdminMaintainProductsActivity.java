a47eda8ae1868c76475991a69973957c34f3fd08
Faizan-Mushtaq <faizan.wani234@gmail.com>
2020-03-22 19:06:43 +0530

    Add Code.

--- app/src/main/java/com/example/ecommerce/AdminMaintainProductsActivity.java
+++ app/src/main/java/com/example/ecommerce/AdminMaintainProductsActivity.java
@@ -3,12 +3,27 @@ package com.example.ecommerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class AdminMaintainProductsActivity extends AppCompatActivity {
+
+    private Button applyChangesBtn, deleteBtn;
+    private EditText name, price, description;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_maintain_products);
+
+        applyChangesBtn = findViewById(R.id.apply_changes_btn);
+        name = findViewById(R.id.product_name_maintain);
+        price = findViewById(R.id.product_price_maintain);
+        description = findViewById(R.id.product_description_maintain);
+        imageView = findViewById(R.id.product_image_maintain);
+        deleteBtn = findViewById(R.id.delete_product_btn);
+
     }
 }

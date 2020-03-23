b74cd5feeb175c07e9e42c00e10efa36db2b5305
Faizan-Mushtaq <faizan.wani234@gmail.com>
2020-03-23 20:57:53 +0530

    Add Admin HomeActivity.

---
+++ app/src/main/java/com/example/ecommerce/Admin/AdminHomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.ecommerce.Admin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ecommerce.R;
+
+public class AdminHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_home);
+    }
+}
